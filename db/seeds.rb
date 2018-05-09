e91dd4d3942f4e2564cc58ccff5fefbbe3659b8a
thinceller <kohei.ct@outlook.jp>
2018-05-09 12:33:18 +0900

    modify seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
-User.create!(email: "example@example.com",
+User.create!(id: 1,
+             email: "example@example.com",
              password: "foobar",
              password_confirmation: "foobar")
 
-Article.create!(title: "First Article", body: "This is a first article.", user_id: 1)
+Article.create!(id: 1, title: "First Article", body: "This is a first article.", user_id: 1)
